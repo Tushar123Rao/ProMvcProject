2e4c5114ba78f9170e69b778b8bd9c1d06f264a0
Tushar Rao <tusharrao21@gmail.com>
2026-03-26 16:06:13 +0530

    Infrastructure: Implemented Repository pattern with IProductRepository

---
+++ ProMvcProject.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProMvcProject.Domain.Entities;
+
+namespace ProMvcProject.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
