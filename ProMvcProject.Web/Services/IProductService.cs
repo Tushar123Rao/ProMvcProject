da7df8416b5157e81921b35d2c92860463a49b5f
Tushar Rao <tusharrao21@gmail.com>
2026-03-26 16:22:05 +0530

    Web: Added Service layer and DTOs for Product handling

---
+++ ProMvcProject.Web/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProMvcProject.Web.DTOs;
+
+namespace ProMvcProject.Web.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAvailableProductsAsync();
+    }
+}
---
+++ ProMvcProject.Web/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProMvcProject.Domain.Interfaces;
+using ProMvcProject.Web.DTOs;
+
+namespace ProMvcProject.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAvailableProductAsync()
+        {
+            var products = await _repository.GetAllAsync();
+            return products.Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price
+            });
+        }
+    }
+}
