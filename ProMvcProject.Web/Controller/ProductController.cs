d114709784640df55a6fda2fd2ab366a838f3f97
Tushar Rao <tusharrao21@gmail.com>
2026-03-26 16:58:21 +0530

    Web: Created ProductController and Index View with Dependency Injection

---
+++ ProMvcProject.Web/Controller/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProMvcProject.Web.Services;
+
+namespace ProMvcProject.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAvailableProductsAsync();
+            // This method looks for a file named Index.cshtml 
+            // inside the Views/Product/ folder
+            return View(products);
+        }
+    }
+}
